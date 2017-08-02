d5cdcd89f04ca98fb8305b5181a02700bf812b60
Oksana Kondratiuk <ksyu@web.de>
2017-08-02 17:46:11 +0200

    home comp. responsive

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,7 +8,6 @@ import { trigger, state, style, animate, transition, keyframes } from '@angular/
   animations: [
       trigger('blurAnimation', [
           state('stable', style({color: 'white'})),
-            // transition('stable => blur', animate('200ms ease-out')),
           state('blur', style({color: '#d0d3d4', transform: 'scale(1, 1)'})),
           transition('blur <=> stable', [
               animate(1000, keyframes([
@@ -24,12 +23,22 @@ import { trigger, state, style, animate, transition, keyframes } from '@angular/
 export class HomeComponent {
     letterNs = [{name: 'C', state: ''}, {name: 'o', state: ''}, {name: 'n', state: ''},
                {name: 't', state: ''}, {name: 'e', state: ''}, {name: 'n', state: ''}, {name: 't', state: ''}];
-   letterFs = [{name: 'D', state: ''}, {name: 'e', state: ''}, {name: 'f', state: ''}, {name: 'i', state: ''}, {name: 'n', state: ''},
+    letterFs = [{name: 'D', state: ''}, {name: 'e', state: ''}, {name: 'f', state: ''}, {name: 'i', state: ''}, {name: 'n', state: ''},
               {name: 'i', state: ''}, {name: 't', state: ''}, {name: 'i', state: ''}, {name: 'o', state: ''}, {name: 'n', state: ''}];
     state: string = 'stable';
     leftN = 75;
     leftF = 0;
-    constructor() {}
+
+    mobile = false;
+
+    onResize($event) {
+        if( innerWidth < 768) {
+        this.mobile = true;
+    } else {
+        this.mobile = false;
+    }
+        console.log('event', innerWidth)
+    }
 
     letteNLeft(i: number) {
         return this.leftN + i*55 + 'px';
@@ -43,4 +52,6 @@ export class HomeComponent {
     backText(letter: any) {
         letter.state = 'stable';
     }
+
+
 }
